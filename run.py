8744d08b7c4bc7350bc48f3736b41ce2735f9a7e
officeyutong <officeyutong@163.com>
2019-08-29 13:41:30 +0800

    显示log

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from main import *
 
 socket.run(web_app, port=config.PORT, host=config.HOST,
-           use_reloader=config.DEBUG, debug=True)
+           use_reloader=config.DEBUG, debug=config.DEBUG, log_output=True)
