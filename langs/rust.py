ea09c0dd2ddea2665f842e1af73ac38411783d15
officeyutong <officeyutong@163.com>
2019-09-11 22:39:56 +0800

    增加Rust支持

---
+++ langs/rust.py
@@ -0,0 +1,14 @@
+# 源程序文件名
+SOURCE_FILE = "{filename}.rs"
+# 输出程序文件名
+OUTPUT_FILE = "{filename}.out"
+# 编译命令行
+COMPILE = "rustc -O -o {output} {source} {extra}"
+# 运行命令行
+RUN = 'sh -c "./{program} {redirect}"'
+# 显示名
+DISPLAY = "Rust 1.33.0"
+# 版本
+VERSION = "rustc 1.33.0"
+# Ace.js模式
+ACE_MODE = "c_cpp"
