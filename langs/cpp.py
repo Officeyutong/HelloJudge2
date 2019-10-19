211fe713d7d8d8e408945e599e87ed3fc96dcf4c
officeyutong <officeyutong@163.com>
2019-10-19 22:51:14 +0800

    qwq

--- langs/cpp.py
+++ langs/cpp.py
@@ -3,11 +3,11 @@ SOURCE_FILE = "{filename}.cpp"
 # 输出程序文件名
 OUTPUT_FILE = "{filename}.out"
 # 编译命令行
-COMPILE = "g++ -Wall -Wextra -std=c++98 -fdiagnostics-color=never -O2 {source} -o {output} {extra}"
+COMPILE = "g++ -Wall -Wextra  -fdiagnostics-color=never -O2 {source} -o {output} {extra}"
 # 运行命令行
 RUN = 'sh -c "./{program} {redirect}"'
 # 显示名
-DISPLAY = "C++(C++ 98)"
+DISPLAY = "C++"
 # 版本
 VERSION = "G++ 8.3"
 # Ace.js模式
