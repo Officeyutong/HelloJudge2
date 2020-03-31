04e8c436bd5c55065a2618b4c512e5e873b3d9b1
officeyutong <officeyutong@163.com>
2020-03-31 15:00:21 +0800

    add demo

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from main import *
-
+# import eventlet
+# eventlet.wsgi.server(
+#     eventlet.wrap_ssl(eventlet.listen(("localhost", 443)),
+#                       certfile='hj2-debug.crt',
+#                       keyfile='hj2-debug.key',
+#                       server_side=True), web_app)
 socket.run(web_app, port=config.PORT, host=config.HOST,
            use_reloader=config.DEBUG, debug=config.DEBUG, log_output=True)
 
