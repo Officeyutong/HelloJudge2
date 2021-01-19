58e96722147f7783d79a041f0d8dc7481d6c9ecc
officeyutong <officeyutong@163.com>
2021-01-19 15:35:46 +0800

    feed bug

--- api/feed.py
+++ api/feed.py
@@ -58,11 +58,15 @@ def rebuild_feed_cache(uid: int):
     if client.exists(key):
         client.delete(key)
     subquery = db.session.query(
-        Follower.target).filter_by(source=uid).subquery()
-    feeds = db.session.query(Feed.id, Feed.uid, Feed.time, Feed.top, Feed.content, User.username, User.email).join(User, User.id == Feed.uid).filter(expr.or_(
+        Follower.target).filter_by(source=uid)
+    # print(f"{SYSTEM_NOTIFICATION_USERID=}")
+    feeds_query = db.session.query(Feed.id, Feed.uid, Feed.time, Feed.top, Feed.content, User.username, User.email).join(User, User.id == Feed.uid).filter(expr.or_(
         Feed.uid == SYSTEM_NOTIFICATION_USERID,  # 所有用户强制关注系统通知
-        Feed.uid == subquery.c.target
-    )).distinct().order_by(Feed.top.desc()).order_by(Feed.time.desc()).limit(config.FEED_STREAM_COUNT_LIMIT).all()
+        Feed.uid.in_(subquery)
+    )).order_by(Feed.top.desc()).order_by(Feed.time.desc()).limit(config.FEED_STREAM_COUNT_LIMIT)
+    # print("query=", feeds_query)
+    feeds = feeds_query.all()
+    # print(feeds)
     for item in feeds:
         # print(item)
         client.rpush(key, json.dumps(
