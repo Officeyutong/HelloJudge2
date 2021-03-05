f12aa4e34bc871333a17884a9460092f8f2e13f9
officeyutong <yt.xyxx@gmail.com>
2021-03-05 23:37:21 +0800

    aes eax

--- common/aes.py
+++ common/aes.py
@@ -5,7 +5,7 @@ import base64
 
 
 def encrypt(key: str, plaintext: str) -> str:
-    aes_obj = AES.new(key.encode("utf-8"), AES.MODE_ECB)
+    aes_obj = AES.new(key.encode("utf-8"), AES.MODE_EAX)
     encoded_bytes = plaintext.encode("utf-8")
     buf = BytesIO()
     # 写上长度用于校验
@@ -16,7 +16,7 @@ def encrypt(key: str, plaintext: str) -> str:
 
 
 def decrypt(key: str, cipher: str) -> str:
-    aes_obj = AES.new(key.encode("utf-8"), AES.MODE_ECB)
+    aes_obj = AES.new(key.encode("utf-8"), AES.MODE_EAX)
     plain = aes_obj.decrypt(base64.decodebytes(cipher.encode("utf-8")))
     data_length = int.from_bytes(plain[:8], "little")
     real_data = plain[8:8+data_length]
