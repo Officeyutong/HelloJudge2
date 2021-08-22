608ad0e87b0d21ce7c0168aed9efb767ca48e3df
officeyutong <yt.xyxx@gmail.com>
2021-08-23 01:35:43 +0800

    fix: route fix

--- routes/view.py
+++ routes/view.py
@@ -298,7 +298,7 @@ def blog_edit(id: int = 1):
 def wiki_config():
     return render_template("wiki/config.html")
 
-@app.route("/wiki/")
+@app.route("/wiki")
 def wiki_page_empty(id=-1):
     return render_template("wiki/page0.html")
 
